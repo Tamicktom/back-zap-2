6ee8c75d0d7eaddd48fa633dfa4d062ef23408d3
Tamicktom <rickpokemon38@gmail.com>
2023-12-06 22:26:30 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+//* Libraries imports
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+//* Create a new Elysia app
+const app = new Elysia();
 
+//* Use cors middleware
+app.use(cors({
+  origin: "*",
+}));
+
+//* Create a new route
+app.get("/", () => "Hello Elysia").listen(3000);
+
+//* Start the app
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
