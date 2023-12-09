31df31fa7a36ef1342758c59a25e034fdf965ebb
Tamicktom <rickpokemon38@gmail.com>
2023-12-09 15:23:07 -0300

    hum

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,49 @@
 //* Libraries imports
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const p = new PrismaClient();
 
 //* Create a new Elysia app
-const app = new Elysia();
+const app = new Elysia({
+  websocket: {
+    idleTimeout: 960,
+  },
+});
 
 //* Use cors middleware
-app.use(cors({
-  origin: "*",
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const schema = z.object({
+  name: z.string(),
+  age: z.number(),
+});
 
 //* Create a new route
-app.get("/", () => "Hello Elysia").listen(3000);
+app.get("/", () => "Hello Elysia");
+
+app.ws("/ws", {
+  message: async (ws, message) => {
+    const parsedMessage = schema.safeParse(message);
+    if (!parsedMessage.success) return ws.send(parsedMessage.error.message);
+    console.log("Message received", ws.data.headers);
+
+    const messages = await p.message.findMany();
+
+    ws.send({
+      messages,
+      message
+    });
+  },
+});
+
+app.listen(3000);
 
 //* Start the app
 console.log(
